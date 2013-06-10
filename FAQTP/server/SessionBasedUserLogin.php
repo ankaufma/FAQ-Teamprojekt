8142024a9cfc2d94ac1046ce2a8819c0575c5449
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-06-10 14:23:24 +0200

    Loginaktualisierung

--- FAQTP/server/SessionBasedUserLogin.php
+++ FAQTP/server/SessionBasedUserLogin.php
@@ -17,7 +17,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 // 	exit( file_get_contents( '/../business/fascade/errorpage.php' ) );
 }
 ?>
-<html>
-<head></head>
-<button class="btn btn-danger btn-small" type="button" onclick="window.location.replace('/../site/Test.php')">cancel</button>
-</html>
