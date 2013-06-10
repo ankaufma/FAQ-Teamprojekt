3c81401b45ba4f94a1c05aa68464772f5800c6a8
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-06-10 16:25:47 +0200

    hubuh

--- FAQTP/server/SessionBasedUserLogin.php
+++ FAQTP/server/SessionBasedUserLogin.php
@@ -1,32 +1,35 @@
 <?php
 include('/../business/fascade/fascade.php');
 $fassi = new Fascade();
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	session_start();
 
-      $username = $_POST['username'];
-      $passwort = $_POST['passwort'];
+	$username = $_POST['username'];
+	$passwort = $_POST['passwort'];
 
-      $hostname = $_SERVER['HTTP_HOST'];
-      $path = dirname($_SERVER['PHP_SELF']);
+	$hostname = $_SERVER['HTTP_HOST'];
+	$path = dirname($_SERVER['PHP_SELF']);
 
-      // Benutzername und Passwort werden überprüft
-      if ($username == 'benjamin' && $passwort == 'geheim') {
-       $_SESSION['angemeldet'] = true;
+	// Benutzername und Passwort werden überprüft
+	if ($username == 'benjamin' && $passwort == 'geheim') {
+		$_SESSION['angemeldet'] = true;
 
-       // Weiterleitung zur geschützten Startseite
-       if ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1') {
-        if (php_sapi_name() == 'cgi') {
-         header('Status: 303 See Other');
-         }
-        else {
-         header('HTTP/1.1 303 See Other');
-         }
-        }
+		// Weiterleitung zur geschützten Startseite
+		if ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1') {
+			if (php_sapi_name() == 'cgi') {
+				header('Status: 303 See Other');
+			}
+			else {
+				header('HTTP/1.1 303 See Other');
+			}
+		}
 
-       header('Location: http://'.$hostname.($path == '/' ? '' : $path).'/index.php');
-       exit;
-       }
-      }
-      print_r($_SESSION);
+		header('Location: http://'.$hostname.($path == '/' ? '' : $path).'/index.php');
+		exit;
+	}
+}
+foreach($_SESSION as $sessionWert){
+	echo $sessionWert;
+	print_r($sessionWert);
+}
 ?>
