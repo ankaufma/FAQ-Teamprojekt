433cf677561c2a0cfe92e199385c9ab9bbd1c9e7
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-06-10 14:43:20 +0200

    ööö

--- FAQTP/server/SessionBasedUserLogin.php
+++ FAQTP/server/SessionBasedUserLogin.php
@@ -16,5 +16,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 // 	header("HTTP/1.1 403 Forbidden");
 // 	exit( file_get_contents( '/../business/fascade/errorpage.php' ) );
 }
- echo $_SESSION.value;
+print_r($_SESSION["berechtigter_User"]);
 ?>
