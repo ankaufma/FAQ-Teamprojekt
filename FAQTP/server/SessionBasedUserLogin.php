3aa84269b16b1a2caba542d58ccf12b3e1b8823a
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-06-10 14:49:45 +0200

    lllll

--- FAQTP/server/SessionBasedUserLogin.php
+++ FAQTP/server/SessionBasedUserLogin.php
@@ -1,20 +1,20 @@
 <?php
 include('/../business/fascade/fascade.php');
 $fassi = new Fascade();
+$userName = $_POST['username'];
+$userPW = $_POST['passwort'];
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	session_start();
-	$userName = $_POST['username'];
-	$userPW = $_POST['passwort'];
 	if($fassi->checkUser($userName,$userPW)==true){
-		if($userName == $_SESSION["berechtigter_User"]){	
-			$_SESSION["berechtigter_User"] = $userName;
+		if($userName == $_SESSION["berechtigter_User"]){
+			print_r($_SESSION["berechtigter_User"]);
 		}else{
 			$_SESSION["berechtigter_User"] = $userName;
+			print_r($_SESSION["berechtigter_User"]);
 		}
 	}
 }else{
 // 	header("HTTP/1.1 403 Forbidden");
 // 	exit( file_get_contents( '/../business/fascade/errorpage.php' ) );
 }
-print_r($_SESSION["berechtigter_User"]);
 ?>
