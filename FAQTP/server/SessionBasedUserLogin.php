db4541f9c045a8bf6265f46217e7de56683970a2
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-06-10 12:36:19 +0200

    Fassi Instanz eingefügt ;-)

--- FAQTP/server/SessionBasedUserLogin.php
+++ FAQTP/server/SessionBasedUserLogin.php
@@ -1,10 +1,11 @@
 <?php
 include('/../business/fascade/fascade.php');
+$fassi = new Fascade();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	session_start();
 	$userName = $_POST['username'];
 	$userPW = $_POST['passwort'];
-	if(checkUser($userName,$userPW)==true){
+	if($fassi->checkUser($userName,$userPW)==true){
 		if ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1') {
 			if (php_sapi_name() == 'cgi') {
 				header('Status: 303 See Other');
