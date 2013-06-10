83a92d4603d95c9165b98ab1d318d2e78e7c1091
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-06-10 16:32:52 +0200

    Aktualisierung

--- FAQTP/server/SessionBasedUserLogin.php
+++ FAQTP/server/SessionBasedUserLogin.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$path = dirname($_SERVER['PHP_SELF']);
 
 	// Benutzername und Passwort werden überprüft
-	if ($username == 'benjamin' && $passwort == 'geheim') {
+	if ($fassi->checkUser($_SESSION['username'],$_SESSION['passwort'])) {
 		$_SESSION['angemeldet'] = true;
 
 		// Weiterleitung zur geschützten Startseite
