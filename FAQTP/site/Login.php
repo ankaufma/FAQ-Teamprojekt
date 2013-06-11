68a1c8546727bcfc21e99f964aa1be36e0a12043
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-06-11 15:25:55 +0200

    jRaty funktioniert endlich ;-)

--- FAQTP/site/Login.php
+++ FAQTP/site/Login.php
@@ -3,13 +3,9 @@
 <title>FAQ-Manager</title>
 <!-- Einbinden des Bootstrap-Stylesheets -->
 <link rel="stylesheet" href="../client/css/bootstrap.min.css">
-
-<!-- optional: Einbinden der jQuery-Bibliothek -->
-	<script	type="text/javascript" src="../client/js/jquery-2.0.0.min.js></script>
- 
-    <!-- optional: Einbinden der Bootstrap-JavaScript-Plugins -->
-	<script type="text/javascript" src="../client/js/jquery.raty.min.js"></script>
-	<script type="text/javascript" src="../client/js/ratyFunctions.js"></script>
+<script type="text/javascript" src="../client/js/jquery-2.0.0.min.js"></script>
+<script type="text/javascript" src="../client/js/jquery.raty.min.js"></script>
+<script type="text/javascript" src="../client/js/ratyFunctions.js"></script>
 	
 </head>
 <body>
