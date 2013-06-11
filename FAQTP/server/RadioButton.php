0cc88845d8e9206df4cfe4c3050126fd8a6c6f66
Hasan <Hasan@house-pc>
2013-06-11 14:48:33 +0200

    update

---
+++ FAQTP/server/RadioButton.php
@@ -0,0 +1,7 @@
+<?php
+?>
+<div>
+	<input type="radio" value="private" checked> Private<br>
+	<input type="radio" value="users only"> Users only<br> 
+	<input type="radio" value="public"> Public<br>
+</div>
