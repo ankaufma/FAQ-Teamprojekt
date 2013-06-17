524d4d98491dfaa6c5de2333f6e7828f1c8f5d89
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-06-17 14:33:31 +0200

    Login Update

--- FAQTP/server/SessionBasedUserLogin.php
+++ FAQTP/server/SessionBasedUserLogin.php
@@ -1,39 +1,43 @@
 <?php
 session_start();
-if(isset($_SESSION['angemeldet'])&& $_SESSION['angemeldet']==0){
-	if(trim($_POST['username']) != ""){
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			include('/../business/fascade/fascade.php');
-			$fassi = new Fascade();
-			$_SESSION['angemeldet']= false;
-			$username = $_POST['username'];
-			$_SESSION['username'] = $username;
-			$userDTO = $fassi->userByUsername($username);
-			$_SESSION['userRole']= $userDTO->getUserrole();
-			$passwort = $_POST['passwort'];
-			$hostname = $_SERVER['HTTP_HOST'];
-			$path = dirname($_SERVER['PHP_SELF']);
-			// Benutzername und Passwort werden überprüft
-			if ($fassi->checkUser($_SESSION['username'],$passwort) == 0) {
-				$_SESSION['angemeldet'] = true;
-				header("Location: ../site/index.php");
-				// Weiterleitung zur geschützten Startseite
-				if ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1') {
-					if (php_sapi_name() == 'cgi') {
-						header('Status: 303 See Other');
+if(isset($_SESSION['angemeldet'])){
+	if($_SESSION['angemeldet']==0){
+		if(trim($_POST['username']) != ""){
+			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+				include('/../business/fascade/fascade.php');
+				$fassi = new Fascade();
+				$_SESSION['angemeldet']= false;
+				$username = $_POST['username'];
+				$_SESSION['username'] = $username;
+				$userDTO = $fassi->userByUsername($username);
+				$_SESSION['userRole']= $userDTO->getUserrole();
+				$passwort = $_POST['passwort'];
+				$hostname = $_SERVER['HTTP_HOST'];
+				$path = dirname($_SERVER['PHP_SELF']);
+				// Benutzername und Passwort werden überprüft
+				if ($fassi->checkUser($_SESSION['username'],$passwort) == 0) {
+					$_SESSION['angemeldet'] = true;
+					header("Location: ../site/index.php");
+					// Weiterleitung zur geschützten Startseite
+					if ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1') {
+						if (php_sapi_name() == 'cgi') {
+							header('Status: 303 See Other');
+						}
+						else {
+							header('HTTP/1.1 303 See Other');
+						}
 					}
-					else {
-						header('HTTP/1.1 303 See Other');
-					}
+				}else{
+					$_SESSION['angemeldet'] = false;
 				}
-			}else{
-				$_SESSION['angemeldet'] = false;
 			}
+		}else{
+			header("Location: errorpage.php");
 		}
 	}else{
-		header("Location: errorpage.php");
+		header("Location: ../site/index.php");
 	}
 }else{
-	header("Location: ../site/index.php");
+	header("Location: errorpage.php");
 }
 ?>
