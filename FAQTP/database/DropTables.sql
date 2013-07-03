82539e166db1eec5dc90ede71fd9d8e1e36fdc38
ankaufma <ankaufma@htwg-konstanz.de>
2013-07-03 11:32:15 +0200

    SQL

---
+++ FAQTP/database/DropTables.sql
@@ -0,0 +1,8 @@
+drop table if exists QuestionAnswer;
+drop table if exists Rating;
+drop table if exists CatQuestion;
+drop table if exists Category;
+drop table if exists Question;
+drop table if exists Comment;
+drop table if exists Answer;
+drop table if exists User;
