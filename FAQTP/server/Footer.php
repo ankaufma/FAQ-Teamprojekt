00a66df8b339a4baec1a8f4b301c14b997655305
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-07-03 11:10:23 +0200

    Update Anzeige Header

--- FAQTP/server/Footer.php
+++ FAQTP/server/Footer.php
@@ -3,7 +3,7 @@
 	<div class="span2"></div>
 	<div class="span8">
 		<p class="lead">FAQ Manager</p>
-		<p>Lovely programming by Andreas Kaufmann, Gian-Lucas Piras, Hasancan
+		<p>Lovely programming by Andreas Kaufmann, Gian-Luca Piras, Hasancan
 			Cifci and Thomas Winter</p>
 		<a href="http://www.google.de">Further Information -> GOOGLE weiss
 			alles</a>
