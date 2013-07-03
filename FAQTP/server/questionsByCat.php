2e28a1ede8536be9d41d5590bfd62a2f0603f512
ankaufma <ankaufma@htwg-konstanz.de>
2013-07-03 10:07:36 +0200

    Merge mit username für Comment

--- FAQTP/server/questionsByCat.php
+++ FAQTP/server/questionsByCat.php
@@ -17,7 +17,10 @@ $k = 0;
 
 $ratings = array();
 $r = 0;
-
+$username = 'Anonymous';
+if(isset($_SESSION['username'])) {
+	$username=$_SESSION['username'];
+}
 
 echo("	<!-- RATING -->
 		<script type=\"text/javascript\" src=\"../client/js/jquery.raty.min.js\"></script>
@@ -93,11 +96,8 @@ foreach($fassi->showQuestionsByCategory($cat) AS $myQs) {
 
 							console.log(\"Answwer-ID: ". $myA->getAnswerId()."\");
 							console.log(\"clicked Score: \" + score);
-							console.log(\"Username: ".$_SESSION['angemeldet']." \");	
-													
-				
+							console.log(\"Username: ".$_SESSION['test']." \");	
 	     				},
-	    	  
 	       				 score: function() {
 	        			 	return $(this).attr('data-score');
 	       				 }
@@ -142,7 +142,7 @@ foreach($fassi->showQuestionsByCategory($cat) AS $myQs) {
 				<textarea name=\"comment\" class=\"span10\" rows=\"8\" placeholder=\"Please enter your comment\"></textarea>
 				<span hidden=\"true\">
 					<input type=\"text\" id=\"answer\" name=\"answer\" value=\"".$myA->getAnswerId()."\"></input>
-					<input type=\"text\" id=\"user\" name=\"user\" value=\"".$_SESSION['username']."\"></input>
+					<input type=\"text\" id=\"user\" name=\"user\" value=\"".$username."\"></input>
 				</span>
 				</div>
 				<div class=\"modal-footer\">
