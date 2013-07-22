1e5420cf22bc5f8c2a48e661ed6a9e21aefb851e
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-07-22 14:47:52 +0200

    Update Passwort Wiederholen wird abgerufen

--- FAQTP/server/RegisterOnServer.php
+++ FAQTP/server/RegisterOnServer.php
@@ -1,21 +1,25 @@
 <html>
-	<head>
-	<meta http-equiv="refresh" content=0"; URL=../site/index.php">
-	</head>
-	<body>
-	</body>
+<head>
+<meta http-equiv="refresh" content=0 "; URL=../site/index.php">
+</head>
+<body>
+</body>
 </html>
 <?php
 if(trim($_POST['user'])!="" && trim($_POST['vorname'])!="" && trim($_POST['nachname'])!="" && trim($_POST['email'])!="" && trim($_POST['passwort'])!=""){
-	include('/../business/fascade/fascade.php');
-	$fassi = new Fascade();
-	$userName = $_POST['user'];
-	$userVorName = $_POST['vorname'];
-	$userNachname = $_POST['nachname'];
-	$userEmail = $_POST['email'];
-	$userPasswort = $_POST['passwort'];
-	$fassi->applyUser($userVorName,$userNachname,$userName,$userEmail,$userPasswort);
+	if($_POST['passwort']== $_POST['passwort2']){
+		include('/../business/fascade/fascade.php');
+		$fassi = new Fascade();
+		$userName = $_POST['user'];
+		$userVorName = $_POST['vorname'];
+		$userNachname = $_POST['nachname'];
+		$userEmail = $_POST['email'];
+		$userPasswort = $_POST['passwort'];
+		$fassi->applyUser($userVorName,$userNachname,$userName,$userEmail,$userPasswort);
 	}else{
-		header("Location: errorpageUnknown.php");
+			header("Location: errorpageUnknown.php");
 	}
+}else{
+	header("Location: errorpageUnknown.php");
+}
 ?>
