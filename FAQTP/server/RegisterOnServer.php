11b2e7e38feea0b2cd1c46a894f5f9f83a54c260
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-07-22 10:29:21 +0200

    Fix Error Handling RegisterOnServer

--- FAQTP/server/RegisterOnServer.php
+++ FAQTP/server/RegisterOnServer.php
@@ -7,6 +7,7 @@ html>
 	</html>
 	
 <?php
+if(trim($_POST['user'])!="" && trim($_POST['vorname'])!="" && trim($_POST['nachname'])!="" && trim($_POST['email'])!="" && trim($_POST['passwort'])!=""){
 	include('/../business/fascade/fascade.php');
 	$fassi = new Fascade();
 	$userName = $_POST['user'];
@@ -15,4 +16,7 @@ html>
 	$userEmail = $_POST['email'];
 	$userPasswort = $_POST['passwort'];
 	$fassi->applyUser($userVorName,$userNachname,$userName,$userEmail,$userPasswort);
+	}else{
+		header("Location: errorpageUnknown.php");
+	}
 ?>
