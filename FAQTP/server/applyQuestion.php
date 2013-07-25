93abe1c4cb86c7f13bb616feaa7dbc4cfc13d0c1
Gian-Luca Piras <gipiras@htwg-konstanz.de>
2013-07-25 17:47:27 +0200

    Erfolgsmeldung beim Absenden einer Frage

--- FAQTP/server/applyQuestion.php
+++ FAQTP/server/applyQuestion.php
@@ -1,59 +1,7 @@
-<html>
-<head>
-<title>FAQ-Manager</title>
-
-<script type="text/javascript" src="../client/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="../client/js/jquery-2.0.0.min.js"></script>
-<script type="text/javascript" src="../client/js/bootstrap.js"></script>
-<script type="text/javascript" src="../client/js/jquery.raty.min.js"></script>
-
-<link rel="stylesheet" href="../client/css/bootstrap.min.css"></link>
-<link rel="stylesheet" href="../client/css/MessageBoxes.css"></link>
-<link rel="stylesheet" href="../client/css/HomeCSS.css"></link>
-
-</head>
-<body>
-	<div class="row-fluid top-buffer">
-		<div class="span4"></div>
-
-		<div class="span4">
-			<h1 class="headline">FAQ Manager</h1>
-		</div>
-		<div class="span4"></div>
-	</div>
-
-	<div class="row-fluid">
-		<div class="span12">
-
-			<div class="row-fluid">
-				<div class="span12 applyQuestionBox">
-					<div class="row-fluid">
-						<div class="span2"></div>
-
-						<div class="span8 applyQuestionText">
-
-							<p class="lead text-success">Your question was sent to the
-								expert!</p>
-							<p class="text-success text">When the expert has answered your
-								question, you will get an email as reminder...</p>
-
-						</div>
-						<div class="span2"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row-fluid">
-		<div class="span12 lastFooter"></div>
-	</div>
-</body>
-</html>
-
 <?php
 include('/../business/fascade/fascade.php');
 $fassi = new Fascade();
 session_start();
 $fassi->applyQuestion($_POST['question'],$_POST['veroeffentlichung'],$_SESSION['username']);
-header("Location: ../site/index.php");
+header("Location: questionSubmit.php");
 ?>
---
+++ FAQTP/server/questionSubmit.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<head>
+<title>FAQ-Manager</title>
+<html>
+<head>
+<title>FAQ-Manager</title>
+<meta http-equiv="refresh" content="2; URL= ../site/index.php">
+
+<script type="text/javascript" src="../client/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../client/js/jquery-2.0.0.min.js"></script>
+<script type="text/javascript" src="../client/js/bootstrap.js"></script>
+<script type="text/javascript" src="../client/js/jquery.raty.min.js"></script>
+
+<link rel="stylesheet" href="../client/css/bootstrap.min.css"></link>
+<link rel="stylesheet" href="../client/css/MessageBoxes.css"></link>
+<link rel="stylesheet" href="../client/css/HomeCSS.css"></link>
+
+</head>
+<body>
+	<div class="row-fluid top-buffer">
+		<div class="span4"></div>
+
+		<div class="span4">
+			<h1 class="headline">FAQ Manager</h1>
+		</div>
+		<div class="span4"></div>
+	</div>
+
+	<div class="row-fluid">
+		<div class="span12">
+
+			<div class="row-fluid">
+				<div class="span12 applyQuestionBox">
+					<div class="row-fluid">
+						<div class="span12"></div>
+
+						<div class="row-fluid">
+							<div class="span4"></div>
+							<div class="span4 errorBlocks">
+								<div class="span8 errorMessage"> 
+									<p class="lead text-error">Your question was</p>
+								<p class="lead text-error">sent to the expert!</p>
+								</div>	
+							</div>
+							<div class="span4"></div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12 lastFooter"></div>
+	</div>
+</body>
+</html>
+</body>
+</html>
