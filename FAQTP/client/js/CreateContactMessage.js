59c0804af0468995d6641df95d2ba46edf2edfce
Thomas <thwinter@htwg-konstanz.de>
2013-07-26 14:05:07 +0200

    Contact mail

--- FAQTP/client/js/CreateContactMessage.js
+++ FAQTP/client/js/CreateContactMessage.js
@@ -1,10 +1,13 @@
 function sendFormular()
 {
+
+	var texteingabe = document.ContactFormular.textfeld.value;
+
 	m = new SendMail();
 	m.To = '<empfaengerName>@<domain>.<end>';
 	m.From = '<absenderName>@<domain>.<end>';
 	m.Subject = 'FAQ Manager - Contact Message';
-	m.Body = TEXT;
+	m.Body = texteingabe;
 	m.send();
 
 	if(m.errorCode() != 0)
